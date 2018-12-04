29cfa86fff096e4b50226f2f41ee2d426f043e65
Kristi Kosta <kristikosta1@gmail.com>
2018-12-04 09:44:01 +0100

    Revert "perkthime"
    This reverts commit 28073b40e9f772be15f43452ab1419c18d1ca819.

--- application/logs/log-2018-11-30.php
+++
@@ -1,4 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2018-11-30 12:03:29 --> Severity: Notice  --> Undefined offset: 0 C:\wamp64\www\pikaturistike\application\views\admin\estate\index.php 230
-ERROR - 2018-11-30 12:03:29 --> Severity: Notice  --> Undefined offset: 0 C:\wamp64\www\pikaturistike\application\views\admin\estate\index.php 230
