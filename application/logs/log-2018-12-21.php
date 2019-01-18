755029ec6204bc370ce7128c4f6c9a859d4f51b6
franciii7 <franciiib7@gmail.com>
2019-01-18 10:30:04 +0100

    ndryshime

---
+++ application/logs/log-2018-12-21.php
@@ -0,0 +1,14 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2018-12-21 08:10:58 --> Severity: Notice  --> Undefined offset: 0 C:\xampp\htdocs\pikaturistike\application\views\admin\user\index.php 111
+ERROR - 2018-12-21 08:10:58 --> Severity: Notice  --> Undefined offset: 0 C:\xampp\htdocs\pikaturistike\application\views\admin\user\index.php 111
+ERROR - 2018-12-21 08:27:42 --> Severity: Notice  --> Undefined offset: 0 C:\xampp\htdocs\pikaturistike\application\views\admin\user\index.php 111
+ERROR - 2018-12-21 08:27:42 --> Severity: Notice  --> Undefined offset: 0 C:\xampp\htdocs\pikaturistike\application\views\admin\user\index.php 111
+ERROR - 2018-12-21 08:28:10 --> Severity: Notice  --> Undefined offset: 0 C:\xampp\htdocs\pikaturistike\application\views\admin\user\edit.php 351
+ERROR - 2018-12-21 08:28:10 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\pikaturistike\application\views\admin\user\edit.php 351
+ERROR - 2018-12-21 08:28:10 --> Severity: Notice  --> Undefined offset: 0 C:\xampp\htdocs\pikaturistike\application\views\admin\user\edit.php 352
+ERROR - 2018-12-21 08:28:10 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\pikaturistike\application\views\admin\user\edit.php 352
+ERROR - 2018-12-21 08:28:10 --> Severity: Notice  --> Array to string conversion C:\xampp\htdocs\pikaturistike\application\views\admin\user\edit.php 370
+ERROR - 2018-12-21 08:28:10 --> Severity: Notice  --> Array to string conversion C:\xampp\htdocs\pikaturistike\application\views\admin\user\edit.php 377
+ERROR - 2018-12-21 08:30:01 --> Severity: Notice  --> Undefined variable: content_language_id C:\xampp\htdocs\pikaturistike\application\views\admin\_layout_main.php 190
+ERROR - 2018-12-21 08:30:01 --> Severity: Notice  --> Undefined property: stdClass::$option_ C:\xampp\htdocs\pikaturistike\application\views\admin\_layout_main.php 190
---
+++ application/logs/log-2018-12-26.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2018-12-26 10:09:33 --> Severity: Notice  --> Undefined index: is_activated C:\xampp\htdocs\pikaturistike\application\controllers\property.php 103
+ERROR - 2018-12-26 10:09:34 --> Severity: Notice  --> Undefined index: is_activated C:\xampp\htdocs\pikaturistike\application\controllers\property.php 103
